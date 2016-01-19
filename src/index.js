8dc7d0eb982049a94ff08977b2bfa0f0d308634c
Brett Jackson <brett@brettjackson.org>
2016-01-19 01:19:49 -0600

    dns stuff

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ var web = require('./web');
 
 var magnetLink = 'magnet:?xt=urn:btih:76baf98e3797c103eb02f668075ad047f10df130&dn=The.Man.from.U.N.C.L.E.2015.1080p.WEB-DL.x264.AC3-JYK&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337';
 
-var browser = mdns.createBrowser(mdns.tcp('googlecast'));
+var sequence = [
+    mdns.rst.DNSServiceResolve(),
+    'DNSServiceGetAddrInfo' in mdns.dns_sd ? mdns.rst.DNSServiceGetAddrInfo() : mdns.rst.getaddrinfo({families:[4]}),
+    mdns.rst.makeAddressesUnique()
+];
+
+var browser = mdns.createBrowser(mdns.tcp('googlecast'), {resolverSequence: sequence});
 
 browser.on('serviceUp', function(service) {
   console.log('found device "%s" at %s:%d', service.name, service.addresses[0], service.port);
